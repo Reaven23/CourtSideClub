a889f90f6f76788e49630ddf184bcc9bf2e63b65
Reaven23 <adrien.regis@gmail.com>
2025-08-02 18:29:33 +0200

    work on articles front

--- config/routes.rb
+++ config/routes.rb
@@ -3,27 +3,27 @@ Rails.application.routes.draw do
   root "pages#home"
   get "dashboard", to: "pages#dashboard"
 
-  # Mini-jeux
   get "mini-jeux", to: "mini_games#index", as: :mini_games
 
-  # Quiz Games
   resources :quiz_games, only: [:show] do
     member do
-      post :start    # Démarre une session de quiz
-      post :answer   # Soumet une réponse
-      post :complete # Finalise le quiz
+      post :start
+      post :answer
+      post :complete
     end
   end
 
-  # Vote campaigns
   resources :vote_campaigns, only: [:index, :show] do
     member do
       post :vote
     end
   end
 
-  # Articles
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  resources :articles, only: [:index, :show, :new, :create, :edit, :update] do
+    member do
+      patch :publish
+    end
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
