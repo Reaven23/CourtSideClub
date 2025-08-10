8ac81c0a19351ae7c2798a47ff8c7bfe0a274b9a
Reaven23 <adrien.regis@gmail.com>
2025-08-10 10:35:58 +0200

    added articles to home page + css for articles section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   before_action :authenticate_user!, only: [:dashboard]
 
   def home
+    @latest_articles = Article.published.order(published_at: :desc).limit(4)
   end
 
   def dashboard
