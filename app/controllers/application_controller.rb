110287ac3d7b3140fe253ad91df593defd66098a
Reaven23 <adrien.regis@gmail.com>
2025-09-02 17:41:32 +0200

    started adding images to pave the way for cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,13 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def configure_permitted_parameters
-    # Paramètres autorisés pour l'inscription
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate])
+        def configure_permitted_parameters
+          # Paramètres autorisés pour l'inscription
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :birthdate, :photo])
 
-    # Paramètres autorisés pour la mise à jour du compte
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthdate])
-  end
+          # Paramètres autorisés pour la mise à jour du compte
+          devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birthdate, :photo])
+        end
 
   def set_unread_notifications_count
     @unread_notifications_count = Notification.where(status: false).count
