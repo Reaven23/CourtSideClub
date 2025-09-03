0a482e688c986743739aba43029ae77df3666741
Reaven23 <adrien.regis@gmail.com>
2025-09-03 13:25:21 +0200

    turbo logic adapted for players

--- app/controllers/admin/players_controller.rb
+++ app/controllers/admin/players_controller.rb
@@ -11,11 +11,19 @@ class Admin::PlayersController < ApplicationController
     @player = Player.new(player_params)
     if @player.save
       respond_to do |format|
-        format.turbo_stream { render turbo_stream: turbo_stream.append('players', partial: 'admin/players/player', locals: { player: @player }) }
+        format.turbo_stream {
+          render turbo_stream: [
+            turbo_stream.append('players', partial: 'admin/players/player', locals: { player: @player }),
+            turbo_stream.update('admin_new_player_form', partial: 'admin/players/form', locals: { player: Player.new })
+          ]
+        }
         format.html { redirect_to admin_dashboard_path(tab: 'players'), notice: 'Joueur créé avec succès.' }
       end
     else
-      render :new
+      respond_to do |format|
+        format.turbo_stream { render turbo_stream: turbo_stream.update('admin_new_player_form', partial: 'admin/players/form', locals: { player: @player }) }
+        format.html { render :new }
+      end
     end
   end
 
@@ -48,6 +56,12 @@ class Admin::PlayersController < ApplicationController
   end
 
   def player_params
-    params.require(:player).permit(:first_name, :last_name, :tournaments_played, :photo)
+    params.require(:player).permit(:first_name, :last_name, :tournament_played, :photo)
+  end
+
+  def ensure_admin!
+    unless current_user&.admin?
+      redirect_to root_path, alert: 'Accès non autorisé.'
+    end
   end
 end
